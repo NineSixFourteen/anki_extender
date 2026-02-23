a540614d5630879fef5fe0ed8683e25bf9fc112e
NineSixFourteen <84684132+NineSixFourteen@users.noreply.github.com>
2026-02-23 00:21:46 +0000

    Fix loading bar

--- src/lib/SendCardPipeline/SendPhrasePipeline.tsx
+++ src/lib/SendCardPipeline/SendPhrasePipeline.tsx
@@ -4,14 +4,19 @@ import { updateStatus, resetStatusBar } from "../StatusBarContextHelper";
 
 export async function sendPhrases(setStatusContext:Function, phrases:Phrases,removeId:Function) {
 
+    const wait = (ms: number) => new Promise(res => setTimeout(res, ms));
   resetStatusBar(setStatusContext)
 
   const pipeline = [
     {
       condition: () => phrases.phrases.length > 0,
-      then: () => {
+      then: async () => {
         updateStatus('CheckRequest',4,'Request is good',setStatusContext)
-        updateStatus('SendImage',2,'',setStatusContext);
+        await wait(1200)
+        updateStatus('SendImage',2,'No Image to send',setStatusContext);
+        await wait(800)
+        updateStatus('SendAudio',3,"Sending Audio to Anki",setStatusContext)
+        await wait(400)
       }, 
       else: () => {
         updateStatus('CheckRequest',0,'Please select some phrases first',setStatusContext)
@@ -20,13 +25,19 @@ export async function sendPhrases(setStatusContext:Function, phrases:Phrases,rem
     },
     {
       condition: async () => await sendAudiosToAnki(phrases,setStatusContext),
-      then: () => {
+      then: async () => {
+        updateStatus('SendAudio',4,"Audio sent succesfully",setStatusContext)
+        await wait(1200)
         updateStatus('SendCard',3,"Sending Cards to Anki",setStatusContext)
+        await wait(400)
       },
       continueIfFail: false
     },
     {
       condition: async () => await sendCardsToAnki(phrases,setStatusContext, removeId),
+      then: () => {
+        updateStatus('SendCard',4,"Cards sent succesfully",setStatusContext)
+      },
       continueIfFail: false
     }
   ]
@@ -34,7 +45,7 @@ export async function sendPhrases(setStatusContext:Function, phrases:Phrases,rem
   //Body that does the pipeline
   for(const stage of pipeline){
     if(await stage.condition() ){
-      if(stage.then) stage.then()
+      if(stage.then) await stage.then()
     } else {
       if(stage.else)stage.else()
       if(!stage.continueIfFail) return;
